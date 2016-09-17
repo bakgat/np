8f94de8cd3f3e6a016b7258bd77c10a00a5ffb9f
bakgat <karl.vaniseghem@klimtoren.be>
2016-09-17 20:56:22 +0200

    Remove ignored files

---
+++ src/env.js
@@ -0,0 +1,18 @@
+(function (window) {
+  window.__env = window.__env || {};
+
+  // API url
+  window.__env.api = 'http://api.klimtoren.be';
+
+  // Base url
+  window.__env.baseUrl = '/';
+
+  // Whether or not to enable debug mode
+  // Setting this to false will disable console output
+  window.__env.enableDebug = true;
+
+  //GOOGLE API Credentials
+  window.__env.googleClientId = '349632514154-qh4289snusgvkrvd4vlkj7g7m7sohaio.apps.googleusercontent.com';
+  window.__env.googleSecret = 'uDK7a6809WyVyBAsTtU5mtCJ';
+
+}(this));
