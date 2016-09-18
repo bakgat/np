35e406363096c823cb596c3e53ca31453b459f5e
bakgat <karl.vaniseghem@klimtoren.be>
2016-09-18 10:21:01 +0200

    Fix(Env): api url

--- src/app/app.module.js
+++ src/app/app.module.js
@@ -20,18 +20,11 @@
             'app.authentication',
             //all global needed services and directives
             'app.global',
-            // 'seed-module',
-            // uncomment above to activate the example seed module
             'app.translate',
             // only need one language?  if you want to turn off translations
             // comment out or remove the 'app.translate', line above
             'app.permission',
             'app.filters',
-            // dont need permissions?  if you want to turn off permissions
-            // comment out or remove the 'app.permission', line above
-            // also remove 'permission' from the first line of dependencies
-            // https://github.com/Narzerus/angular-permission see here for why
-            //'app.examples',
             'app.dashboards',
             'app.manage',
             'app.care',
@@ -48,9 +41,7 @@
 
     .config(function(RestangularProvider, $mdDateLocaleProvider) {
         //RestangularProvider.setDefaultHttpFields({cache: true});
-        RestangularProvider.setBaseUrl('http://localhost:4000');
-
-
+        RestangularProvider.setBaseUrl(env.api);
     });
 
 })();
