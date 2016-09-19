56d0162bbe99553a89959f8d549c24b325fe27c8
bakgat <karl.vaniseghem@klimtoren.be>
2016-09-19 13:05:11 +0200

    Update(Avatar): styling the avatars

--- src/app/global/profile/user-avatar.directive.js
+++ src/app/global/profile/user-avatar.directive.js
@@ -37,17 +37,21 @@
     function UserAvatarController($filter) {
         var vm = this;
         vm.user = vm.ngModel;
-        vm.source = null;
+
+        vm.style = '';
 
         init();
         ////////////////////
 
         function init() {
-            vm.source = 'http://schkt.volglvs.be/PIX/';
-            vm.source += vm.user.lastName.removeDiacritics(true).replace(' ', '%20');
-            vm.source += vm.user.firstName.removeDiacritics(true).replace(' ', '%20');
-            vm.source += $filter('date')(vm.user.birthday, 'dd.MM.yyyy');
-            vm.source += '.JPG';
+            var source = 'http://schkt.volglvs.be/PIX/';
+            source += vm.user.lastName.removeDiacritics(true).replace(' ', '%20');
+            source += vm.user.firstName.removeDiacritics(true).replace(' ', '%20');
+            source += $filter('date')(vm.user.birthday, 'dd.MM.yyyy');
+            source += '.JPG';
+
+            vm.style = 'url(' + source + ')';
+            console.log(vm.style);
         }
     }
 
