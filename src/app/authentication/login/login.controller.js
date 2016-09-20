29eaeb1ec99af4669353e334edc275f761a204f5
bakgat <karl.vaniseghem@klimtoren.be>
2016-09-20 15:38:06 +0200

    Update: event tracking via auth_token

--- src/app/authentication/login/login.controller.js
+++ src/app/authentication/login/login.controller.js
@@ -6,18 +6,20 @@
         .controller('LoginController', LoginController);
 
     /* @ngInject */
-    function LoginController($rootScope, $state, googleService, triSettings, UserService) {
+    function LoginController($rootScope, $state, googleService, triSettings, UserService, Restangular) {
         var vm = this;
         vm.loginClick = loginClick;
 
         vm.triSettings = triSettings;
         init();
         ////////////////    
-        function init() {
-        }
+        function init() {}
 
         function loginClick() {
-            googleService.login().then(function(data) {
+            googleService.login().then(function(user) {
+                Restangular.setDefaultRequestParams({
+                    auth_token: user.id
+                });
                 //console.log($rootScope.$previousState);
                 $state.go('triangular.analytics');
             }, function(err) {
