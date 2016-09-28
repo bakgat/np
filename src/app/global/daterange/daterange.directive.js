f3ef87f20bc142394c5fde0e0bfe7dcfac4d1191
bakgat <karl.vaniseghem@klimtoren.be>
2016-09-28 14:39:39 +0200

    Build v 0.1.0

---
+++ src/app/global/daterange/daterange.directive.js
@@ -0,0 +1,150 @@
+(function() {
+    'use strict';
+
+    angular
+        .module('app.global')
+        .directive('mdDaterange', mdDaterange);
+
+    /* @ngInject */
+    function mdDaterange() {
+        var directive = {
+            link: link,
+            restrict: 'E',
+            scope: {
+                range: '=',
+                onDaterangeChanged: '&'
+            },
+            templateUrl: 'app/global/daterange/daterange.tmpl.html',
+            controller: DateRangeController,
+            controllerAs: 'vm',
+            bindToController: true
+        };
+        return directive;
+        ////////////////////////
+
+        function link($scope, $element, $attrs) {}
+    }
+
+    /* @ngInject  */
+    function DateRangeController($mdPanel, $rootScope, DateRangeService, HTTPCache, moment) {
+        var vm = this;
+
+        vm.showMenu = showMenu;
+
+
+        init();
+        /////////////////////////
+
+        function init() {
+            vm.range = DateRangeService.range();
+            setRequestParams();
+        }
+
+        function showMenu(ev) {
+            var position = $mdPanel.newPanelPosition()
+                .relativeTo('.datepicker-label')
+                .addPanelPosition($mdPanel.xPosition.ALIGN_START, $mdPanel.yPosition.BELOW);
+
+            var config = {
+                attachTo: angular.element(document.body),
+                controller: DateRangePickerController,
+                controllerAs: 'vm',
+                templateUrl: 'app/global/daterange/daterange-picker.tmpl.html',
+                panelClass: 'demo-menu-example',
+                position: position,
+                locals: {
+                    range: vm.range,
+                    onClose: closePicker
+                },
+                openFrom: ev,
+                clickOutsideToClose: true,
+                escapeToClose: true,
+                focusOnOpen: false,
+                zIndex: 2
+            };
+
+            $mdPanel.open(config);
+
+
+        }
+
+        function closePicker(data) {
+            vm.range = data.range;
+            setRequestParams();
+            vm.onDaterangeChanged && vm.onDaterangeChanged();
+        }
+
+        function setRequestParams() {
+            HTTPCache.setDefaultRequestParams({
+                start: vm.range.start.format('YYYY-MM-DD'),
+                end: vm.range.end.format('YYYY-MM-DD')
+            });
+        }
+
+
+
+    }
+
+
+    /* @ngInject */
+    function DateRangePickerController($scope, mdPanelRef, range, onClose, DateRangeService) {
+        var vm = this;
+
+        vm.range = range;
+
+        vm.select = select;
+        vm.selectCustomStart = selectCustomStart;
+        vm.selectCustomEnd = selectCustomEnd;
+
+        vm.cancel = cancel;
+
+        init();
+        /////////////////////////
+        function init() {
+
+        }
+
+        function select(option) {
+            if (option == 'week') {
+                vm.range = DateRangeService.thisWeek();
+            } else if (option == 'month') {
+                vm.range = DateRangeService.thisMonth();
+            } else if (option == 'quarter') {
+                vm.range = DateRangeService.thisQuarter();
+            } else if (option == 'schoolyear') {
+                vm.range = DateRangeService.thisSchoolyear();
+            }
+            saveAndClose();
+        }
+
+        function cancel() {
+            mdPanelRef.close();
+        }
+
+        function saveAndClose() {
+            mdPanelRef.close().then(function(mdPanelRef) {
+                onClose({
+                    range: vm.range
+                });
+            });
+        }
+
+        function selectCustomStart() {
+            vm.range.start = moment(vm.start);
+            vm.selectedTab = 1;
+        }
+
+        function selectCustomEnd() {
+            vm.range.end = moment(vm.end);
+            DateRangeService.custom(vm.range);
+            saveAndClose();
+        }
+
+        $scope.$watch('vm.range', function() {
+            vm.start = vm.range.start.toDate();
+            vm.end = vm.range.end.toDate();
+        }, true);
+
+    }
+
+})();
---
+++ src/app/global/daterange/daterange.factory.js
@@ -0,0 +1,106 @@
+(function() {
+    'use strict';
+
+    angular
+        .module('app.global')
+        .factory('DateRangeService', DateRangeService);
+
+    /* @ngInject */
+    function DateRangeService($q, $timeout, $rootScope, moment) {
+
+
+        var service = {
+            thisWeek: thisWeek,
+            thisMonth: thisMonth,
+            thisQuarter: thisQuarter,
+            thisSchoolyear: thisSchoolyear,
+            custom: custom,
+            range: range
+        };
+
+        var range = undefined;
+
+        return service;
+        //////////////////////////////
+
+        function range() {
+            if(range === undefined) {
+                range = thisQuarter();
+            }
+            return range;
+        }
+
+        function custom(cr) {
+            range = cr;
+            return range;
+        }
+
+        function thisWeek() {
+            range = {
+                start: moment().startOf('week'),
+                end: moment().endOf('week')
+            };
+            return range;
+        }
+
+        function thisMonth() {
+            range = {
+                start: moment().startOf('month'),
+                end: moment().endOf('month')
+            };
+            return range;
+        }
+
+        function thisQuarter() {
+            var now = moment();
+
+            if (now.month() > 7) {
+                return quarter(1);
+            } else if (now < moment().easter()) {
+                return quarter(2);
+            } else {
+                return quarter(3);
+            }
+        }
+
+        function quarter(q) {
+            var start, end;
+            //q = 1, 2, 3
+            if (q == 1) {
+                //First schoolQuarter
+                start = moment().month(8).startOf('month');
+                end = moment().month(11).endOf('month');
+            } else if (q == 2) {
+                //Second schoolQuarter
+                start = moment().startOf('year');
+                end = moment().easter();
+            } else {
+                start = moment().easter();
+                end = moment().month(5).endOf('month');
+            }
+            range = {
+                start: start,
+                end: end
+            };
+            return range;
+        }
+
+        function thisSchoolyear() {
+            var start, end, now = moment();
+            var yearNow = now.year();
+            if (now.month() < 8) {
+                start = moment().subtract(-1, 'year').month(8).startOf('month');
+                end = moment().month(7).endOf('month');
+            } else {
+                start = moment().month(8).startOf('month');
+                end = moment().add(1, 'year').month(7).endOf('month');
+            }
+            range = {
+                start: start,
+                end: end
+            };
+            return range;
+        }
+
+    }
+})();
