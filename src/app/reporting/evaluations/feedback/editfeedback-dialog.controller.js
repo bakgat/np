d6492372dbf2c5e2a9efd2a61c4b3b6dd2022e85
Karl Van Iseghem <karl.vaniseghem@icloud.com>
2017-06-24 14:06:21 +0200

    Build v0.8.1

--- src/app/reporting/evaluations/feedback/editfeedback-dialog.controller.js
+++ src/app/reporting/evaluations/feedback/editfeedback-dialog.controller.js
@@ -7,7 +7,7 @@
 
     /* @ngInject */
     function EditFeedbackDialogController($scope, $timeout, $mdDialog, $filter, _, triSkins, result, focusOnOpen,
-        ReportingService, StudentService, textAngularManager, $sanitize) {
+        ReportingService, StudentService, textAngularManager, $sanitize, _env, DateRangeService, UserService, $http) {
 
         var vm = this;
         vm.cancel = cancel;
@@ -32,10 +32,21 @@
         init();
         ///////////////
 
+
         function init() {
-            ReportingService.one('student', result.student.id).get().then(function(response) {
+            console.log('init');
+            var range = DateRangeService.range();
+            var rangeQuery = '&qstart=' + moment(range.start).format('YYYY-MM-DD') + '&qend=' + moment(range.end).format('YYYY-MM-DD');
+            var tokenQuery = '&token=' + UserService.getCurrentUser().auth_token;
+            $http.get(_env.api + '/report/student?id=' + result.student.id + rangeQuery + tokenQuery)
+                .then(function(response) {
+                    console.log(response.data);
+                    makeReport(response.data);
+                });
+            /*ReportingService.one('student').get({id: result.student.id}).get().then(function(response) {
+                console.log(response);
                 makeReport(response);
-            });
+            });*/
         }
 
         function cancel() {
@@ -63,7 +74,7 @@
                 var fMaxRaw = 0;
                 var tMaxRaw = 0;
                 angular.forEach(major.branches, function(branch) {
-                    var cur = branch.history[0];
+                    var cur = branch.current;
                     if (cur.permanent) {
                         permRaw += cur.permanent;
                         pMaxRaw += cur.max;
@@ -76,7 +87,31 @@
                         totalRaw += cur.total;
                         tMaxRaw += cur.max;
                     }
+
+                    /*
+                     * CHARTS
+                     */
+                     var markers = [];
+                        angular.forEach(branch.history, function(item) {
+                            var marker = (item.total / item.max) * 100;
+                            markers.push(marker);
+                        });
+
+                        
+
+                        if (markers.length > 1) {
+                            //reverse markers because they are ordered DESC
+                            markers.reverse();
+                            //generate url for google charts.
+                            //this API is deprecated but the only hack for now
+                            var url = 'http://chart.apis.google.com/chart?chs=75x30&chd=t:' +
+                                markers.join(',') +
+                                '&cht=lc:nda&chm=o,0066FF,0,-1,4|o,FFFFFF,0,-1,2&chma=2,2,2,2';
+                            branch.chartUrl = url;
+                        }
+                       
                 });
+
                 major.permanent = (permRaw / pMaxRaw) * 100;
                 major.final = (finalRaw / fMaxRaw) * 100;
                 major.total = (totalRaw / tMaxRaw) * 100;
